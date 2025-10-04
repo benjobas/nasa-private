749610e0ef8f5ea98d80098742f84da7d71df475
benjobas <166581907+benjobas@users.noreply.github.com>
2025-10-04 19:51:02 -0300

    Cambios para vercel

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const probabilitiesRouter = require('./routes/probabilities');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 
 app.get('/', (_req, res) => {
